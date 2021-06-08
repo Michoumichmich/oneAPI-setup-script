60b2cd96985fcb90013d88679e614e110ee74670
Michoumichmich <50529895+Michoumichmich@users.noreply.github.com>
2021-06-08 14:27:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -47,7 +47,8 @@ python3 ./buildbot/configure.py \
  --cmake-opt="-DCMAKE_INSTALL_PREFIX=$DPCPP_HOME/deploy" \
  --cmake-opt="-DCUDA_SDK_ROOT_DIR=$CUDA_ROOT" \
  --cmake-opt="-DLLVM_ENABLE_PROJECTS=clang;sycl;llvm-spirv;opencl;opencl-aot;libdevice;xpti;xptifw;libclc;openmp;clang-tools-extra;compiler-rt" \
- --cmake-opt="-DLLVM_BUILD_TESTS=$cmake_test"
+ --cmake-opt="-DLLVM_BUILD_TESTS=$cmake_test" \
+ --cmake-opt="-DCMAKE_CXX_STANDARD=20"
 cd build
 ninja install -j `nproc`
 if  $run_test ; then 
